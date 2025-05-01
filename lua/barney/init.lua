16759e265500bd34ecea988cc21cb78fec6393dd
Michael Barney, Jr <mbarneyme@gmail.com>
2025-05-01 13:41:23 -0400

    test

--- lua/barney/init.lua
+++ lua/barney/init.lua
@@ -1,2 +1,11 @@
 require("barney.core")
 require("barney.plugins")
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function()
+    local local_lua_file = vim.fn.getcwd() .. "/.nvim.lua"
+    if vim.fn.filereadable(local_lua_file) == 1 then
+      vim.cmd("source " .. local_lua_file)
+    end
+  end,
+  desc = "Source local .nvim.lua configuration files",
+})
